24e00a10f36fea85b6eb33e1395be159c0bdd63f
Himanshu-soulspark <udbhavscience12@gmail.com>
2025-08-20 04:27:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Render जैसे प्लेटफॉर्म PORT एनवायरमेंट वेरिएबल का उपयोग करते हैं
 
+app.get('/', (req, res) => {
+  res.send('Hello from Soul Spark!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
